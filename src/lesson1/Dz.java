5bab4d60bf879f224bdd0fd59392a9b6f71fa04e
Svetik41rus <sova0076@mail.ru>
2021-11-18 18:10:12 +1200

    дз по первому уроку

--- src/lesson1/Dz.java
+++ src/lesson1/Dz.java
@@ -3,8 +3,52 @@ package lesson1;
 public class Dz {
 
     public static void main(String[] args) {
+        // System.out.println("Orange\nBanana\nApple");
+        // первый пункт
+        //printThreeWords();
+        // checkSumSign();
+        //printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
+        // второе задание
 
-            }
+    }
 
-}
+    public static void checkSumSign() {
+        int a = 10, b = 20, c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 45, b = 30;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+
+        }
+    }
+    }
+
