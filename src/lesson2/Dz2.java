fb0447d94d4cab6a24b06532c79f4040b3b458d5
Svetik41rus <sova0076@mail.ru>
2021-11-21 20:55:00 +1200

    дз по второму уроку

--- src/lesson2/Dz2.java
+++ src/lesson2/Dz2.java
@@ -2,9 +2,12 @@ package lesson2;
 
 public class Dz2 {
     public static void main(String[] args) {
-        System.out.println(summ(6,5));
-
+        System.out.println(summ(6, 5));
+        PolOrNegativ(-1);
+        System.out.println(number(5));
+        TextNumber("Привет",10);
     }
+
     //первое задание
     public static boolean summ(int a, int b) {
         int summ = a + b;
@@ -15,8 +18,33 @@ public class Dz2 {
         }
 
     }
+
     // второе задание
+    public static void PolOrNegativ(int a) {
+        if (a >= 0) {
 
+            System.out.println("Положительное");
+        } else {
 
+            System.out.println("Отрицательное");
+        }
+    }
+
+    //третье здание
+    public static boolean number(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //четвертое задание
+    public static void TextNumber(String slovo, int number) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(slovo  + i);
+        }
+
+    }
 }
 
