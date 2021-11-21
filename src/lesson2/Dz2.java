685d09a24006a7a5af6b6dcd42e1ac6b5b09653b
Svetik41rus <sova0076@mail.ru>
2021-11-21 19:24:12 +1200

    дз по второму уроку

--- src/lesson2/Dz2.java
+++ src/lesson2/Dz2.java
@@ -2,9 +2,21 @@ package lesson2;
 
 public class Dz2 {
     public static void main(String[] args) {
-    }
-
-    public static void test(String[] args) {
+        System.out.println(summ(6,5));
 
     }
+    //первое задание
+    public static boolean summ(int a, int b) {
+        int summ = a + b;
+        if (summ > 10 && summ <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    // второе задание
+
+
 }
+
