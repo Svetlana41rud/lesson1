0a4842cfe910139092c5923f93012b8e552bf3d4
Svetik41rus <sova0076@mail.ru>
2021-12-02 00:17:55 +1200

    дз по пятому уроку

---
+++ src/lesson5/Employee.java
@@ -0,0 +1,28 @@
+package lesson5;
+
+public class Employee {
+    public String name;
+    public String position;
+    public String email;
+    public String phone;
+    public int salary;
+    public int age;
+
+public Employee (String name, String position, String email, String phone, int salary, int age) {
+    this.name = name;
+    this.position = position;
+    this.email = email;
+    this.phone = phone;
+    this.salary = salary;
+    this.age = age;
+};
+
+    public void info()
+    {
+        System.out.printf("Employee: %s %s %s %s %s %d\n", name, position, email, phone, salary, age);
+    }
+
+
+}
+
+
