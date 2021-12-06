3317893e5e51269f53faef7d28262fcba5cc0c4f
Svetik41rus <sova0076@mail.ru>
2021-12-06 23:05:36 +1200

    дз по пятому уроку исправленное

--- src/lesson5/Employee.java
+++ src/lesson5/Employee.java
@@ -1,25 +1,30 @@
 package lesson5;
 
 public class Employee {
-    public String name;
-    public String position;
-    public String email;
-    public String phone;
-    public int salary;
-    public int age;
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
 
-public Employee (String name, String position, String email, String phone, int salary, int age) {
-    this.name = name;
-    this.position = position;
-    this.email = email;
-    this.phone = phone;
-    this.salary = salary;
-    this.age = age;
-};
+    public Employee(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 
-    public void info()
-    {
+    ;
+
+    public void info() {
         System.out.printf("Employee: %s %s %s %s %s %d\n", name, position, email, phone, salary, age);
+    }
+
+    int getAge() {
+        return age;
     }
 
 
--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -2,17 +2,16 @@ package lesson5;
 
 public class Main {
     public static void main(String[] args) {
-//        Employee employee = new Employee("Ivanov Ivan Ivanich", "engineer", "mail@mail.ru", "415-255", 40000, 35);
-//        employee.info();
-        Employee[] employees = new Employee[] {
-            new Employee("Ivanov Ivan Ivanich", "engineer", "mail@mail.ru", "415-100", 60000, 35),
-                    new Employee("Ivanov Ivan Ivanich", "engineer1", "2mail@mail.ru", "415-646", 40000, 50),
-                    new Employee("Ivanov Ivan Ivanich", "engineer", "6mail@mail.ru", "415-255", 50000, 40),
-                    new Employee("Ivanov Ivan Ivanich", "engineer2", "7mail@mail.ru", "415-600", 40000, 45),
-                    new Employee("Ivanov Ivan Ivanich", "engineer", "mail@mail.ru", "415-200", 40000, 25)
+
+        Employee[] employees = new Employee[]{
+                new Employee("Ivanov Ivan Ivanich", "engineer", "mail@mail.ru", "415-100", 60000, 35),
+                new Employee("Ivanov Ivan Ivanich", "engineer1", "2mail@mail.ru", "415-646", 40000, 50),
+                new Employee("Ivanov Ivan Ivanich", "engineer", "6mail@mail.ru", "415-255", 50000, 40),
+                new Employee("Ivanov Ivan Ivanich", "engineer2", "7mail@mail.ru", "415-600", 40000, 45),
+                new Employee("Ivanov Ivan Ivanich", "engineer", "mail@mail.ru", "415-200", 40000, 25)
         };
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i].age > 40)  employees[i].info();
+            if (employees[i].getAge() > 40) employees[i].info();
 
         }
 
